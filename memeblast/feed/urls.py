c59481b809df8c9c40fc539c8944d1054f8f88c9
Sigve Sebastian Farstad <sigvefarstad@gmail.com>
2013-04-12 12:46:19 +0200

    Fixes feed after directory shuffling

--- memeblast/feed/urls.py
+++ memeblast/feed/urls.py
@@ -1,7 +1,7 @@
-from django.conf.urls import patterns,include,url
-from feed.models import Picture
-from feed import views
+from django.conf.urls import patterns, url
+import feed.views as views
 
-urlpatterns = patterns('',
-        url(r'^$', 'views.whatever_function_you_want_to_use_to_display_shit'),
+urlpatterns = patterns(
+    '',
+    url(r'^$', views.feed),
 )
