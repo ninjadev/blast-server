cfe34a51aa0b0eb0034a9cd26b0f6660fe04959f
cristeahub <christoffer.tonnessen@gmail.com>
2013-04-12 13:31:11 +0200

    enables django admin

--- memeblast/memeblast/settings.py
+++ memeblast/memeblast/settings.py
@@ -126,7 +126,7 @@ INSTALLED_APPS = (
     'django_toolbar',
 
     # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
+     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
 
