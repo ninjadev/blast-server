1ed1ca1f3d79459a9d4fc56647cdb8f819b664dc
cristeahub <christoffer.tonnessen@gmail.com>
2013-04-12 15:41:25 +0200

    fix structure of model

--- memeblast/feed/models.py
+++ memeblast/feed/models.py
@@ -1,8 +1,6 @@
 from django.db import models
 
-
 class Picture(models.Model):
     picture_url = models.CharField(max_length=100)
     posted_on = models.DateTimeField()
-    topText = models.CharField(max_length=20, null=True, blank=True)
-    bottomText = models.charField(max_length=20, null=True, blank=True)
+    text = models.CharField(max_length=20, null=True, blank=True)
