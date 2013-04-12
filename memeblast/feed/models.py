ba88781e0a733a4f1ea37435629210c7b67e0f95
Asbjørn Ottesen Steinskog <asbjorn@steinskog.me>
2013-04-12 15:37:03 +0200

    Adds fields to picture class.

--- memeblast/feed/models.py
+++ memeblast/feed/models.py
@@ -4,4 +4,5 @@ from django.db import models
 class Picture(models.Model):
     picture_url = models.CharField(max_length=100)
     posted_on = models.DateTimeField()
-    text = models.CharField(max_length=1024, null=True, blank=True)
+    topText = models.CharField(max_length=20, null=True, blank=True)
+    bottomText = models.charField(max_length=20, null=True, blank=True)
