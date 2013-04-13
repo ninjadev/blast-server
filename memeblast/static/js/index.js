94520d2c8611eece3c13033210ac89354ac05219
Sigve Sebastian Farstad <sigvefarstad@gmail.com>
2013-04-13 02:49:43 +0200

    Readded alerts

--- memeblast/static/js/index.js
+++ memeblast/static/js/index.js
@@ -100,6 +100,7 @@ function getPic(data) {
 }
 
 function sendImage(data) {
+    alert("attempting to send data beginning with " + data.slice(0,10,0));
     data.length && $.ajax({ 
         type: "POST", 
         url: "/upload/post/", 
@@ -108,8 +109,10 @@ function sendImage(data) {
         },
         success: function(data) {
             image_id = data['image_id'];
+            alert(image_id);
         },
         error: function() {
+            alert("massive failure");
         }
     })
 }
@@ -122,8 +125,10 @@ function  publishImage(image_id) {
             "image_id":image_id
         },
         success: function(data) {
+            alert("published image"); 
         },
         error: function() {
+            alert("massive failure");
         }
     })
     window.location = "/";
