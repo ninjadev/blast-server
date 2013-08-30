cf4e9a1660e41958e0c70b24cebea242ca2e0491
Stian Jensen <me@stianj.com>
2013-08-31 00:48:21 +0200

    Show image in Django admin

--- memeblast/feed/models.py
+++ memeblast/feed/models.py
@@ -15,6 +15,10 @@ class Picture(models.Model):
     def get_absolute_url(self):
         return settings.MEDIA_URL + '/' + self.picture_url
 
+    def admin_image(self):
+        return '<img src="%s" style="width:300px"/>' % self.get_absolute_url()
+    admin_image.allow_tags = True
+
     @staticmethod
     def get_latest(count=30):
         return Picture.objects.all().order_by('-posted_on')[:30]
